3abc373765f946bd87725fd075cb3858d50b70a6
vermanica <veronicaann621@gmail.com>
2022-11-18 14:55:26 -0500

    Finished homework 3

--- R_Programming/Homework/SQL_Homework_3_Winter.R
+++ R_Programming/Homework/SQL_Homework_3_Winter.R
@@ -1,4 +1,5 @@
 ##############################X
+#--Programming for Ecologists-X
 #------ SQL homework 3 -------X
 #--------- 11/15/2022 --------X
 #----------- VAW -------------X
@@ -20,14 +21,15 @@ library(RSQLite)
 # Establihs connection to DB ----
 # connection
 con <- dbConnect(drv = RSQLite::SQLite(), "SQL/lohr_and_haag.db")
-
+dbListTables(con)
+dbListFields(con, "lohr_and_haag")
 # a) (10pts) Compute the min, max, and average reproductive output for each 
 # population and output with information on what size lake each population is 
 # from (large or small).  
 
-calc <- dbGetQuery(con, 'SELECT size, AVG(ro) AS average, MIN(ro) AS min, 
+calc <- dbGetQuery(con, 'SELECT size, AVG(ro) AS mean, MIN(ro) AS min, 
                                 MAX(ro) AS max FROM lohr_and_haag
-                         GROUP BY size')
+                   GROUP BY size')
 calc
 
 
@@ -37,14 +39,35 @@ calc
 unq <- dbGetQuery(con, 'SELECT DISTINCT pop FROM lohr_and_haag')
 unq
 
-del <- dbSendStatement(con, 'DELETE FROM lohr_and_haag WHERE "pop" = :x')
-dbBind(del, params = list(x = "VOL", "VRI")) 
+del <- dbExecute(con, 'DELETE FROM lohr_and_haag WHERE pop LIKE 
+                       "V%" OR pop LIKE "K%"')
+del
+
+# check
+ck <- dbGetQuery(con, 'SELECT DISTINCT pop FROM lohr_and_haag')
+ck
 
 # c) (10pts) Iteratively (preferably with an implicit or explicit loop) query 
 #     which populations (of those that are left) have a longer lifespan 
 #      on average than 40 days, 50 days, 60 days, and 70 days. 
 
-
+# ages to loop over
+ages <- c(40, 50, 60, 70)
+# query statement
+longer <- dbSendQuery(con, 'SELECT pop, AVG(ad) FROM lohr_and_haag 
+                      GROUP BY pop HAVING AVG(ad) > :x')
+# loop
+for(i in ages){
+  
+  # bind statement for above query
+  dbBind(longer, params = list(x = i))
+  
+  # print title statement and subsequent results
+  print(paste("pop with avg lifespan > ", i))
+  print(dbFetch(longer))
+  
+  #Done
+}
 
 # Question 2 ----
 # 2) (20pts) Revisiting the Comte et al. (2016) database once again, do the following 
@@ -61,12 +84,12 @@ together
 
 # b) (10pts) What is the count of extirpations from P1 to P2 for northern pike (lucius) 
 # for each basin? This should be done using SQL code within Python or R. 
-count <- dbGetQuery(con2, 'SELECT COUNT(Per1) AS extripations_P1, Basin
-                    FROM together WHERE Per1 = "P1"
+count <- dbGetQuery(con2, 'SELECT Basin, COUNT(lucius) AS luciusEXT
+                    FROM together WHERE lucius = "extirpated" AND Per1 = "P1" AND Per2 = "P2"
                     GROUP BY Basin')
 count
 
-
+# Done!
 
 
 
